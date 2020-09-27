5e53eab15281954db68bc6a951960d5e526c495a
jcdelano <junio.caesar@gmail.com>
2020-09-28 04:18:00 +0700

    Penambahan Bermacam File dan Folder
    templates/index.html
    static/css/style.css
    static/js/script.js
    static/img/logoAlfamart.svg
    
    Output:
    Tampilan awal

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("R")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug = True)
